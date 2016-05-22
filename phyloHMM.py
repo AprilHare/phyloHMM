117f01e53e8831e742b4bbb648b45be61e5db813
luke <luke.rast@gmail.com>
2016-05-22 17:03:42 -0400

    implements phyloTree model

--- phyloHMM.py
+++ phyloHMM.py
@@ -3,12 +3,66 @@
 import numpy as ny
 from hmmlearn import hmm
 
-class phyloHMM(object):
+class phyloHMM(hmm._BaseHMM):
     """phyloHMM is a subclass of hmmlearn hmms for which the emissions are phylogenetic models"""
-    def __init__(self, arg):
-        super(phyloHMM, self).__init__()
-        self.arg = arg
-        
+    def __init__(self, n_components=1, covariance_type='diag',
+                 min_covar=1e-3,
+                 startprob_prior=1.0, transmat_prior=1.0,
+                 means_prior=0, means_weight=0,
+                 covars_prior=1e-2, covars_weight=1,
+                 algorithm="viterbi", random_state=None,
+                 n_iter=10, tol=1e-2, verbose=False,
+                 params="stmc", init_params="stmc"):
+
+        # this function initializes the hmmm object itself.
+
+        hmm._BaseHMM.__init__(self, n_components,
+                          startprob_prior=startprob_prior,
+                          transmat_prior=transmat_prior,
+                          algorithm=algorithm,
+                          random_state=random_state,
+                          n_iter=n_iter, tol=tol, verbose=verbose,
+                          params=params, init_params=init_params)
+
+
+    # functions that we must overwrite from the _BaseHMM class
+    def _init(self, X, length):
+        # this function initalizes the model parameters for HMM prior to doing the fitting
+        pass
+
+    def _check(self):
+        # validatese model parameters prior to fitting
+        pass
+
+    def _generate_sample_from_state(self, state, random_state=None):
+        # Generates a random sample from a given component.
+        pass
+
+    def _compute_log_likelihood(self, X):
+        # Computes per-component log probability under the model.
+        pass
+
+    def _initialize_sufficient_statistics():
+        #Initializes sufficient statistics required for M-step.
+        pass
+
+    def _accumulate_sufficient_statistics(stats, X, framelogprob, posteriors, fwdlattice, bwdlattice):
+        #Updates sufficient statistics from a given sample.
+        pass
+
+    def _do_mstep(stats):
+        # Performs the M-step of EM algorithm.
+        pass
+
+
+
+
+
+
+
+
+
+
 
 
 
--- phyloTree.py
+++ phyloTree.py
@@ -1,5 +1,94 @@
 #implements a phylogenetic tree class
 
+import numpy as np
+
+
+class phyloTree(object):
+    """phyloTree is a phylogenetic tree"""
+    def __init__(self, treeSchema):
+        # phylo-tree is a matrix of connection lengths with a collection of other useful functions.
+        # Everything disconnected is set to zero, and only updated by multiplication
+        super(phyloTree, self).__init__()
+
+        def parseSchema(treeSchema):
+            """parses the tree depth first. The schema is assumed to be a tuple of tuples"""
+
+            def count(current):
+                if type(current) is tuple:
+                    # we are still recursing.
+                    currCount = 0
+                    for entry in current:
+                        currCount += count(entry)
+                    return currCount + 1
+
+                else:
+                    #this is taken to be the condition for the end of the tree.
+                    return 1
+
+            numNodes = count(treeSchema)
+            out = np.zeros( (numNodes, numNodes ) )
+
+
+            def parser(current, bigIndex, out):
+                #recurser
+                if type(current) is tuple:
+                    # we are still recursing.
+                    myIndex = bigIndex
+                    running = myIndex + 1
+                    for entry in current:
+                        out[myIndex, running] = 1
+                        out[running, myIndex] = 1
+
+                        biggest = parser( entry, running, out)
+                        running = biggest + 1
+
+                    return running - 1
+
+                else:
+                    #this is taken to be the condition for the end of the tree.
+                    myIndex = bigIndex
+                    return myIndex
+
+            parser( treeSchema, 0, out)
+
+            return out
+
+        # the edges tell us the branch lengths of the tree and the structure of the tree
+        self.edges = parseSchema( treeSchema)
+
+        # nucleotide rate matrices
+        self.background = 0.25 * np.ones( (4,1) )   # background rates
+        self.transition = 0.25 * np.ones((4,4))     # transition rates
+
+
+
+
+    def checkParams(self):
+        """ checks to make sure that the paramter values are legal"""
+        if abs( sum(self.background) - 1.0 ) > 1e-3:
+            raise Exception( "Background rates must sum to 1")
+        for row in range( self.transition.shape[0]):
+            if abs( sum( self.transition[row, :] ) - 1.0 ) > 1e-3:
+                raise Exception( "Transistion rates must sum to 1. Row " + str(row) )
+
+
+    def getConnections(self, index):
+        (out,) = np.where( self.edges[index, :] != 0 )
+        return out
+
+
+    def setLength(self, location, value):
+        # only update if the entry is non-zero
+        i1, i2 = location
+
+        prev1 = self.edges[i1,i2];
+        prev2 = self.edges[i2,i1];
+
+        if prev1 == 0 or prev2 == 0:
+            raise Exception( 'Incompatible with tree schema')
+        else:
+            self.edges[i1,i2] = value
+            self.edges[i2,i1] = value
 
 
 
